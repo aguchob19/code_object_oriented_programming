d7cb829eda1ebd38e7230b9d14da5147dc120866
aguch <agucho_b19@hotmail.com>
2019-04-22 23:18:23 +0200

    intance variables

--- class_file.py
+++ class_file.py
@@ -1,12 +1,16 @@
 # create a class
 class DataShell():
     # initialize class with self argument
-    def __init__(self):
-        pass
+    def __init__(self, integerInput):
+        # Set data as instance variable, and assign the value of integerInput
+        self.data = integerInput
 
 
-# instantiate Datashell
-my_data_shell = DataShell()
+# declare variable
+x = 10
+
+# instantiate DataShell
+my_data_shell = DataShell(x)
 
 # print data shel
-print(my_data_shell)
+print(my_data_shell.data)
