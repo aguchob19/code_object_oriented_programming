70520f9894f55ab849587613917ad0298c3d6d45
aguch <agucho_b19@hotmail.com>
2019-04-22 23:29:11 +0200

    Class Variables

--- class_file.py
+++ class_file.py
@@ -1,21 +1,21 @@
 # create a class
 class DataShell():
-    # initialize class with self argument
-    def __init__(self, identifier, data):
-        # Set identifier and data as instance variables, assigning value of input arguments
+
+    # Declare a class variable family, and assign value of "DataShell"
+    family = 'DataShell'
+
+    # initialize class with self argument, identifier arguments
+    def __init__(self, identifier):
+        # Set identifier as instance variable of input argument
         self.identifier = identifier
-        self.data = data
 
 
 # declare variable
 x = 10
-y = [1, 2, 3, 4, 5]
 
 # instantiate DataShell
-my_data_shell = DataShell(x, y)
+my_data_shell = DataShell(x)
 
 # print data shell identifier
-print(my_data_shell.identifier)
+print(my_data_shell.family)
 
-# print data shell data
-print(my_data_shell.data)
