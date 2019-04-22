625d716a4185cbee344326500f184cdacbae3e91
aguch <agucho_b19@hotmail.com>
2019-04-22 23:10:03 +0200

    the init method

--- class_file.py
+++ class_file.py
@@ -1,4 +1,12 @@
 # create a class
 class DataShell():
-    pass
-# this is other test
+    # initialize class with self argument
+    def __init__(self):
+        pass
+
+
+# instantiate Datashell
+my_data_shell = DataShell()
+
+# print data shel
+print(my_data_shell)
