7a272cdc5fb0606c7a896378414e00106fd2f27f
aguch <agucho_b19@hotmail.com>
2019-04-22 23:58:42 +0200

    methods 2

--- class_file.py
+++ class_file.py
@@ -1,16 +1,20 @@
 # create a class
 class DataShell():
-    # Initialize class with self argument
-    def __init__(self):
-        pass
+    # Initialize class with self and dataList as arguments
+    def __init__(self, dataList):
+        # Set data as instance variable, and assign it the value of dataList
+        self.data = dataList
 
-    # Define class method which takes self argument: print_static
-    def print_static(self):
-        # Print string
-        print("You just executed a class method!")
+    # Define class method which takes self argument: show
+    def show(self):
+        print(self.data)
 
-# Instantiate DataShell taking no arguments: my_data_shell
-my_data_shell = DataShell()
 
-# Call the print_static method of your newly created object
-my_data_shell.print_static()
+# Declare variable with list of integers from 1 to 10: integer_list
+integer_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Instantiate DataShell taking integer_list as argument: my_data_shell
+my_data_shell = DataShell(integer_list)
+
+# Call the show method of your newly created object
+my_data_shell.show()
