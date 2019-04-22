e5c576c952880477f356b637554d522f690bb665
aguch <agucho_b19@hotmail.com>
2019-04-22 23:22:27 +0200

    Multiple Instance Variables

--- class_file.py
+++ class_file.py
@@ -1,16 +1,21 @@
 # create a class
 class DataShell():
     # initialize class with self argument
-    def __init__(self, integerInput):
-        # Set data as instance variable, and assign the value of integerInput
-        self.data = integerInput
+    def __init__(self, identifier, data):
+        # Set identifier and data as instance variables, assigning value of input arguments
+        self.identifier = identifier
+        self.data = data
 
 
 # declare variable
 x = 10
+y = [1, 2, 3, 4, 5]
 
 # instantiate DataShell
-my_data_shell = DataShell(x)
+my_data_shell = DataShell(x, y)
 
-# print data shel
+# print data shell identifier
+print(my_data_shell.identifier)
+
+# print data shell data
 print(my_data_shell.data)
