2852528467b04b2ae3ef5df9d350f116b8b33ff5
aguch <agucho_b19@hotmail.com>
2019-04-23 00:05:10 +0200

    methods 3

--- class_file.py
+++ class_file.py
@@ -9,6 +9,11 @@ class DataShell():
     def show(self):
         print(self.data)
 
+    # Define method that prints average of data: avg
+    def avg(self):
+        avg = sum(self.data) / float(len(self.data))
+        print(avg)
+
 
 # Declare variable with list of integers from 1 to 10: integer_list
 integer_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -16,5 +21,6 @@ integer_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 # Instantiate DataShell taking integer_list as argument: my_data_shell
 my_data_shell = DataShell(integer_list)
 
-# Call the show method of your newly created object
+# Call the show and avg methods of your newly created object
 my_data_shell.show()
+my_data_shell.avg()
