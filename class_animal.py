65943524db6a072610cfb00725b14522253e27f6
aguch <agucho_b19@hotmail.com>
2019-04-23 15:47:49 +0200

    Vertebrate Inheritance

--- class_animal.py
+++ class_animal.py
@@ -1,19 +1,22 @@
-# Create a class Animal
-class Animal:
+# Create a class Vertebrate
+class Vertebrate:
+    spinal_cord = True
     def __init__(self, name):
         self.name = name
 
 
-# Create a class Mammal, which inherits from Animal
-class Mammal(Animal):
+# Create a class Mammal, which inherits from Vertebrate
+class Mammal(Vertebrate):
     def __init__(self, name, animal_type):
         self.animal_type = animal_type
+        self.temperature_regulation = True
 
 
-# Create a class Reptile, which also inherits from Animal
-class Reptile(Animal):
+# Create a class Reptile, which also inherits from Vertebrate
+class Reptile(Vertebrate):
     def __init__(self, name, animal_type):
         self.animal_type = animal_type
+        self.temperature_regulation = False
 
 
 # Instantiate a mammal with name 'Daisy' and animal_type 'dog': daisy
@@ -22,6 +25,10 @@ daisy = Mammal('Daisy', 'dog')
 # Instantiate a reptile with name 'Stella' and animal_type 'alligator': stella
 stella = Reptile('Stela', 'alligator')
 
-# Print both objects
-print(daisy)
-print(stella)
+# Print stella's attributes spinal_cord and temperature_regulation
+print("Stella Spinal cord: " + str(stella.spinal_cord))
+print("Stella temperature regularization: " + str(stella.temperature_regulation))
+
+# Print daisy's attributes spinal_cord and temperature_regulation
+print("Daisy Spinal cord: " + str(daisy.spinal_cord))
+print("Daisy temperature regularization: " + str(daisy.temperature_regulation))
