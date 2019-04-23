f426625105891b6781c849e62f7cc38a1d5f1656
aguch <agucho_b19@hotmail.com>
2019-04-23 12:21:26 +0200

    Renaming columns

--- class_file.py
+++ class_file.py
@@ -10,15 +10,24 @@ class DataShell:
         self.filepath = filepath
         self.data_as_csv = pd.read_csv(filepath)
 
+    # Define method rename_column, with arguments self, column_name, and new_column_name
+    def rename_column(self, column_name, new_column_name):
+        self.data_as_csv.columns = self.data_as_csv.columns.str.replace(column_name, new_column_name)
+
 
 us_life_expectancy = r'data_sets\us_life_expectancy.csv'
 
 # Instantiate DataShell with us_life_expectancy as input argument
 us_data_shell = DataShell(us_life_expectancy)
 
-# Print df
-print(us_data_shell.data_as_csv)
+# Define method rename_column, with arguments self, column_name, and new_column_name
+print(us_data_shell.data_as_csv.dtypes)
 
+# Rename your objects column 'code' to 'country_code'
+us_data_shell.rename_column('code', 'country_code')
+
+# Again, print the datatype of your object's data_as_csv attribute
+print(us_data_shell.data_as_csv.dtypes)
 
 # # Define method that returns data: show
 # def show(self):
