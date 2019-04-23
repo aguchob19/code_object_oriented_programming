1f87993fabb12b3a373b896fecf0759a943add34
aguch <agucho_b19@hotmail.com>
2019-04-23 16:13:57 +0200

    Composition and Inheritance I

--- data_shell_class.py
+++ data_shell_class.py
@@ -5,20 +5,29 @@ import pandas as pd
 
 # Create class: DataShell
 class DataShell:
-    def __init__(self, inputFile):
-        self.file = inputFile
+    # Class variable family
+    family = 'DataShell'
+
+    # Initialization method with arguments, and instance variables
+    def __init__(self, name, filepath):
+        self. name = name
+        self.filepath = filepath
 
 
 # Create class CsvDataShell, which inherits from DataShell
 class CsvDataShell(DataShell):
-    def __init__(self, inputFile):
-        self.inputFile = inputFile
-        self.data = pd.read_csv(inputFile)
+    # Initialization method with arguments self, name, filepath
+    def __init__(self, name, filepath):
+        # Instance variable data
+        self.data = pd.read_csv(filepath)
+        # Instance variable stats
+        self.stats = self.data.describe()
+
 
 us_life_expectancy = r'data_sets\us_life_expectancy.csv'
 
 # Instantiate CsvDataShell as us_data_shell, passing us_life_expectancy as argument
-us_data_shell = CsvDataShell(us_life_expectancy)
+us_data_shell = CsvDataShell("US", us_life_expectancy)
 
 # Print my_data_shell
-print(us_data_shell.data)
+print(us_data_shell.stats)
