da942a288fc1a6276a02346246c91e55b7d72996
aguch <agucho_b19@hotmail.com>
2019-04-23 12:24:35 +0200

    Self-Describing DataShells

--- class_file.py
+++ class_file.py
@@ -14,6 +14,9 @@ class DataShell:
     def rename_column(self, column_name, new_column_name):
         self.data_as_csv.columns = self.data_as_csv.columns.str.replace(column_name, new_column_name)
 
+    # Define get_stats method, with argument self
+    def get_stats(self):
+        return self.data_as_csv.describe()
 
 us_life_expectancy = r'data_sets\us_life_expectancy.csv'
 
@@ -21,22 +24,4 @@ us_life_expectancy = r'data_sets\us_life_expectancy.csv'
 us_data_shell = DataShell(us_life_expectancy)
 
 # Define method rename_column, with arguments self, column_name, and new_column_name
-print(us_data_shell.data_as_csv.dtypes)
-
-# Rename your objects column 'code' to 'country_code'
-us_data_shell.rename_column('code', 'country_code')
-
-# Again, print the datatype of your object's data_as_csv attribute
-print(us_data_shell.data_as_csv.dtypes)
-
-# # Define method that returns data: show
-# def show(self):
-#     # return
-#     return self.data
-#
-#
-# # Define method that prints average of data: avg
-# def avg(self):
-#     avg = sum(self.data) / float(len(self.data))
-#     # returnd
-#     return avg
+print(us_data_shell.get_stats())
