537764499d23d4214da883d497cdc62f9834e167
aguch <agucho_b19@hotmail.com>
2019-04-23 12:16:13 +0200

    Data as Attributes

--- class_file.py
+++ class_file.py
@@ -5,26 +5,19 @@ import pandas as pd
 # create a class
 class DataShell:
     # Initialize class with self and dataList as arguments
-    def __init__(self, inputFile):
+    def __init__(self, filepath):
         # Set data as instance variable, and assign it the value of dataList
-        self.file = inputFile
-
-    # Define generate_csv method, with self argument
-    def generate_csv(self):
-        self.data_as_csv = pd.read_csv(self.file)
-        return self.data_as_csv
+        self.filepath = filepath
+        self.data_as_csv = pd.read_csv(filepath)
 
 
 us_life_expectancy = r'data_sets\us_life_expectancy.csv'
 
 # Instantiate DataShell with us_life_expectancy as input argument
-data_shell = DataShell(us_life_expectancy)
-
-# Call data_shell's generate_csv method, assign it to df
-df = data_shell.generate_csv()
+us_data_shell = DataShell(us_life_expectancy)
 
 # Print df
-print(df)
+print(us_data_shell.data_as_csv)
 
 
 # # Define method that returns data: show
