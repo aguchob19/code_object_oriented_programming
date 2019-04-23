b7a02d89159ecacc00aa5727d28dc72b849dee29
aguch <agucho_b19@hotmail.com>
2019-04-23 16:26:11 +0200

    Composition and Inheritance II

--- data_shell_class.py
+++ data_shell_class.py
@@ -24,10 +24,24 @@ class CsvDataShell(DataShell):
         self.stats = self.data.describe()
 
 
+# Define class TsvDataShell
+class TsvDataShell(DataShell):
+    # Initialization method with arguments self, name, filepath
+    def __init__(self, name, filepath):
+        # Instance variable data
+        self.data = pd.read_csv(filepath, sep='\t')
+        # Instance variable stats
+        self.stats = self.data.describe()
+
+
 us_life_expectancy = r'data_sets\us_life_expectancy.csv'
 
-# Instantiate CsvDataShell as us_data_shell, passing us_life_expectancy as argument
+# Instantiate CsvDataShell as us_data_shell, print us_data_shell.stats
 us_data_shell = CsvDataShell("US", us_life_expectancy)
-
-# Print my_data_shell
 print(us_data_shell.stats)
+
+france_life_expectancy = r'data_sets\france_life_expectancy.txt'
+
+# Instantiate TsvDataShell as france_data_shell, print france_data_shell.stats
+france_data_shell = TsvDataShell("France", france_life_expectancy)
+print(france_data_shell.stats)
